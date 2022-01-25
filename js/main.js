d6075fce80803f067798a3454d907af25b405cf3
michaelb2001 <brunco.michael@gmail.com>
2022-01-25 14:46:37 +0100

    genera 10 email da api

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 let vm = new Vue({
     el:'#app',
     data:{
-        mails: []
+        mails: [],
+        numeroMailDaGenerare : 10
+    },
+    methods:{
+        aggiungiEmail:function(){
+            for(let i=0; i < this.numeroMailDaGenerare; i++){
+                this.mails=[];
+                let mail ="";
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((risposta) => {
+                    console.log(risposta);
+                    mail = risposta.data.response;
+                    this.mails.push(mail);
+                })
+                .catch(function (error){
+                    console.log(error);
+                });
+                
+            }
+ 
+  
+        }
     },
     mounted  : function() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((risposta) => {
-                console.log(risposta);
-            })
-            .catch(function (error){
-                console.log(error);
-            });
+
         }
 });
